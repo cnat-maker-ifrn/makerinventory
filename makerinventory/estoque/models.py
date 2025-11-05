c24c0334d3471245ab334143b453aa56a9c9985b
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-05 16:00:43 -0300

    feat: criar e testar models de categoria e subcategoria

--- makerinventory/estoque/models.py
+++ makerinventory/estoque/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
+class Subcategoria(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    categoria = models.ForeignKey(Categoria, related_name="subcategorias", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Subcategoria"
+        verbose_name_plural = "Subcategorias"
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
