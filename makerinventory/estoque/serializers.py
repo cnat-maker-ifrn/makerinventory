7de8efe1703ab0c14249e59010ae7a5c82e01e80
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-07 17:08:13 -0300

    feat: criar serializers(iniciais)

---
+++ makerinventory/estoque/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import *
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class SubcategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subcategoria
+        fields = '__all__'
+
+class ProdutoUnitarioSerializer(serializers.ModelSerializer):
+    quantidade_em_estoque = serializers.ReadOnlyField()
+
+    class Meta:
+        model = ProdutoUnitario
+        fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+class ProdutoFracionadoSerializer(serializers.ModelSerializer):
+    quantidade_em_estoque = serializers.ReadOnlyField()
+
+    class Meta:
+        model = ProdutoFracionado
+        fields = '__all__'
+
+class LoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lote
+        fields = '__all__'
+
+class SolicitanteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Solicitante
+        fields = '__all__'
+
+class EmprestimoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Emprestimo
+        fields = '__all__'
+
+class MovimentacaoEstoqueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovimentacaoEstoque
+        fields = '__all__'
