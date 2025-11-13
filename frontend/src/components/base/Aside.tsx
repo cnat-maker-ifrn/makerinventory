56032c83d149c49bc30cd69df0fc10d3e4c18561
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-13 17:55:13 -0300

    feat: implementar inicialmente o sidebar

---
+++ frontend/src/components/base/Aside.tsx
@@ -0,0 +1,15 @@
+export default function Aside(){
+    return(
+        <aside className="bg-[]">
+            <nav>
+                <ul>
+                    <li>Dashboard</li>
+                    <li>Produtos</li>
+                    <li>Saídas</li>
+                    <li>Empréstimos</li>
+                    <li>Movimentações</li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
