ad272d28d8171836cf3e2b166060dd277d8031a0
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-14 15:34:12 -0300

    feat: trocar ícones e arquivos de logo

--- frontend/src/components/base/Header.tsx
+++ frontend/src/components/base/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <header className="w-full h-[100px] bg-white flex items-center px-4 mb-2 shadow-md">
-      <div className="flex items-center gap-4 pl-3.5">
+      <div className="flex items-center gap-4 pl-[10px]">
         <img 
-
-          src="/assets/logo-inventariomaker.png" 
+          className="w-[85px] h-[85px]"
+          src="/assets/logo-inventariomaker.svg" 
           alt="Logo Inventário Maker"
         />
 
